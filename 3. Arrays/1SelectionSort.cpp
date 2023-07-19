aaa8c7216a121ba01483c5752cc47c91f35ff8ab
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-07-19 18:44:47 +0530

    Selection Sort

---
+++ 3. Arrays/1SelectionSort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if(minIndex != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+
+    int *arr = new int[n];
+
+    cout << "Enter the elements of the array : ";
+
+    for (int i = 0; i < n;i++)
+    {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+
+    cout << "The sorted array is : ";
+
+    for (int i = 0; i < n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
