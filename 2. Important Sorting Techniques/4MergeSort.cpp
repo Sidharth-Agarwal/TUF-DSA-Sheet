66a1832bd574fd0b5a888ef560d8ddb400e508fe
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-07-19 22:46:24 +0530

    New

---
+++ 2. Important Sorting Techniques/4MergeSort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+void mergeSort(int *arr, int n)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+
+    int *arr = new int[n];
+
+    cout << "Enter the elements of the array : ";
+
+    for (int i = 0; i < n;i++)
+    {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, n);
+
+    cout << "The sorted array is : ";
+
+    for (int i = 0; i < n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
