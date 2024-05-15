a6eab4828121f4dd7c5216db2c45ee9b477a90a7
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-05-15 21:50:53 +0530

    Basic maths

---
+++ 1. Basics/4. Basic Maths/7PrintAllDivisions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int* printDivisors(int n, int &size)
+{
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+
+
+}
