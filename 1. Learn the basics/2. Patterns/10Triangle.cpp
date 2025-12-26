595ab65402ccd78c20887d939c1980e0536ba5ca
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2025-12-26 23:39:35 +0530

    update

---
+++ 1. Learn the basics/2. Patterns/10Triangle.cpp
@@ -0,0 +1,33 @@
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of rows : ";
+    cin >> n;
+
+    for(int i=1; i<=(n*2)-1; i++) {
+        int stars = i;
+
+        if(i>n) {
+            stars = (2*n)-i;
+        }
+
+        for(int j=1; j<=stars; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
